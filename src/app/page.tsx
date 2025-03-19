ffc4cfb744bc6e80d0daeddc82a96fba940b1bc3
mnkhbldd <mnkhbldd@gmail.com>
2025-03-19 23:19:06 +0800

    Nothing to notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { Features } from "@/components/Features";
-import { NavigationBar } from "@/components/NavigationBar";
-import Image from "next/image";
+import { NavigationBar, Nowplaying } from "@/components";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col items-center">
-      <div className="w-full h-full flex flex-col gap-[24px]">
+      <div className="w-full h-full flex flex-col gap-[24px] items-center">
         <NavigationBar />
-        <Features />
+        <Nowplaying />
       </div>
-      <p>Hello</p>
+      <p>Test</p>
     </div>
   );
 }
