051590642455652ba2a7a0559a75048a6246aa1f
24LP4054 24LP4054 <24LP4054@G302-CC0093.local>
2025-04-07 09:57:12 +0800

    test

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -86,103 +86,103 @@ function Genres() {
   };
 
   return (
-    <Suspense> <div className="flex gap-[32px] flex-col px-24">
-    <p className="text-[30px] font-semibold text-black">Search filter</p>
-    <div className="flex items-start">
-      <div>
-        <div className="w-[387px] flex flex-col gap-3">
-          <div className="flex flex-col gap-1">
-            <p className="text-[24px] font-semibold text-black">Genres</p>
-            <p className="text-[16px] text-black font-normal">
-              See lists of movies by genre
+    
+      <div className="flex gap-[32px] flex-col px-24">
+        <p className="text-[30px] font-semibold text-black">Search filter</p>
+        <div className="flex items-start">
+          <div>
+            <div className="w-[387px] flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
+                <p className="text-[24px] font-semibold text-black">Genres</p>
+                <p className="text-[16px] text-black font-normal">
+                  See lists of movies by genre
+                </p>
+              </div>
+              <div className="flex gap-4 flex-wrap">
+                {movieGenres.map((value) => (
+                  <Badge
+                    onClick={() => handleBadgeClick(value.id)}
+                    key={value.id}
+                    variant="outline"
+                    className={
+                      selectedGenres.includes(value.id)
+                        ? "bg-black text-white"
+                        : "bg-white text-black"
+                    }
+                  >
+                    {value.name}{" "}
+                    {selectedGenres.includes(value.id) ? <X /> : <ArrowRight />}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="h-screen w-[32px]  px-10"></div>
+          <div>
+            {" "}
+            <p className="text-[24px] font-semibold text-black">
+              {movies.length} titles in "
+              {movieGenres
+                .filter((g) => selectedGenres.includes(g.id))
+                .map((g) => g.name)
+                .join(", ") || "Unknown Genre"}
+              "
             </p>
-          </div>
-          <div className="flex gap-4 flex-wrap">
-            {movieGenres.map((value) => (
-              <Badge
-                onClick={() => handleBadgeClick(value.id)}
-                key={value.id}
-                variant="outline"
-                className={
-                  selectedGenres.includes(value.id)
-                    ? "bg-black text-white"
-                    : "bg-white text-black"
-                }
-              >
-                {value.name}{" "}
-                {selectedGenres.includes(value.id) ? <X /> : <ArrowRight />}
-              </Badge>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      <div className="h-screen w-[32px]  px-10"></div>
-      <div>
-        {" "}
-        <p className="text-[24px] font-semibold text-black">
-          {movies.length} titles in "
-          {movieGenres
-            .filter((g) => selectedGenres.includes(g.id))
-            .map((g) => g.name)
-            .join(", ") || "Unknown Genre"}
-          "
-        </p>
-        <div className="flex flex-wrap gap-10 py-10 max-w-[1760px] w-fit just ">
-          {" "}
-          {movies.slice(0, 12).map((value, index) => (
-            <MovieCard
-              isSmall={false}
-              className=""
-              onClick={() => handleOnclick(value.id)}
-              key={index}
-              title={value.title}
-              vote_average={value.vote_average}
-              poster_path={value.poster_path}
-            />
-          ))}
-          <Pagination className="flex justify-end">
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  href={`?genres=${selectedGenres.join(",")}&page=${
-                    pageNumber - 1
-                  }`}
+            <div className="flex flex-wrap gap-10 py-10 max-w-[1760px] w-fit just ">
+              {" "}
+              {movies.slice(0, 12).map((value, index) => (
+                <MovieCard
+                  isSmall={false}
+                  className=""
+                  onClick={() => handleOnclick(value.id)}
+                  key={index}
+                  title={value.title}
+                  vote_average={value.vote_average}
+                  poster_path={value.poster_path}
                 />
-              </PaginationItem>
-
-              {movies.map((value, index) => (
-                <div key={index}>
-                  <PaginationItem>
-                    <PaginationLink
-                      isActive={pageNumber === index + 1}
-                      href={`?genres=${selectedGenres.join(",")}&page=${
-                        index + 1
-                      }`}
-                    >
-                      {index + 1}
-                    </PaginationLink>
-                  </PaginationItem>
-                </div>
               ))}
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
+              <Pagination className="flex justify-end">
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      href={`?genres=${selectedGenres.join(",")}&page=${
+                        pageNumber - 1
+                      }`}
+                    />
+                  </PaginationItem>
 
-              <PaginationItem>
-                <PaginationNext
-                  href={`?genres=${selectedGenres.join(",")}&page=${
-                    pageNumber + 1
-                  }`}
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
+                  {movies.map((value, index) => (
+                    <div key={index}>
+                      <PaginationItem>
+                        <PaginationLink
+                          isActive={pageNumber === index + 1}
+                          href={`?genres=${selectedGenres.join(",")}&page=${
+                            index + 1
+                          }`}
+                        >
+                          {index + 1}
+                        </PaginationLink>
+                      </PaginationItem>
+                    </div>
+                  ))}
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+
+                  <PaginationItem>
+                    <PaginationNext
+                      href={`?genres=${selectedGenres.join(",")}&page=${
+                        pageNumber + 1
+                      }`}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div></Suspense>
-     
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { Nowplaying, Popular, TopRated, Upcoming } from "@/components";
 import * as React from "react";
 
 export default function Home() {
-  return (
-    <div className="h-auto  w-full flex flex-col items-center gap-[52px] py-[52px]">
-      <Nowplaying />
-      <Upcoming />
-      <Popular />
-      <TopRated />
-    </div>
+  return ( 
+    <React.Suspense> 
+      <div className="h-auto  w-full flex flex-col items-center gap-[52px] py-[52px]">
+        <Nowplaying />
+        <Upcoming />
+        <Popular />
+        <TopRated />
+      </div>
+    </React.Suspense>
+   
   );
 }
