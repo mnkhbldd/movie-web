366c5dc0c742d0f399b8a4d60e011ada7393c058
24LP4054 24LP4054 <24LP4054@G302-CC0093.local>
2025-04-07 09:58:22 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,15 @@ import * as React from "react";
 
 export default function Home() {
   return ( 
-    <React.Suspense> 
-      <div className="h-auto  w-full flex flex-col items-center gap-[52px] py-[52px]">
+    
+    <div className="h-auto  w-full flex flex-col items-center gap-[52px] py-[52px]">
+      <React.Suspense>
         <Nowplaying />
         <Upcoming />
         <Popular />
         <TopRated />
-      </div>
-    </React.Suspense>
-   
+      </React.Suspense>
+     
+    </div>
   );
 }
